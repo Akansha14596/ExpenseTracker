327355d83404d93a7558cbbdca5c27057b6a9916
Akansha14596 <Akanshadocuments@gmail.com>
2023-10-23 11:21:10 +0530

    Add data using props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Electricity bill",
+      amount: "5k",
+      date: new Date(2023, 10, 10),
+    },
+    {
+      id: "e2",
+      title: "Car insurance",
+      amount: "10k",
+      date: new Date(2023, 9, 6),
+    },
+    {
+      id: "e3",
+      title: "Home loan",
+      amount: "5000k",
+      date: new Date(2022, 7, 5),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expense Manager</h1>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
